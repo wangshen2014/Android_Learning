9273c1332a92e36825c5ab55eda294398baef151
someone <someone@someplace.com>
2017-04-15 16:35:14 +0800

    day1:how to write a simple dial program

---
+++ day1/1/phonedail/app/src/main/java/com/example/administrator/phonedail/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.administrator.phonedail;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity {
+    private static String TAG = "MainActivity";
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button b = (Button) findViewById(R.id.button);
+        TextView textView = (TextView) findViewById(R.id.textView);
+        editText = (EditText) findViewById(R.id.editText);
+
+        b.setOnClickListener(new MyClickListener());
+
+    }
+
+
+
+    private class MyClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view){
+            Editable text = editText.getText();
+            String s  = text.toString().trim();
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_CALL);
+            intent.setData(Uri.parse("tel:"+s));
+            startActivity(intent);
+
+            Log.i(TAG,"text.toString()" + s);
+        }
+    }
+}
