9bad34a4d6ed5f73a76d14376e86eb5d9af2576c
someone <someone@someplace.com>
2017-04-16 12:17:38 +0800

    day2 project:a simple login system

---
+++ day2/Login/app/src/main/java/com/example/administrator/login/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.administrator.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    private String TAG = MainActivity.class.getSimpleName();
+    private EditText editTextName;
+    private EditText editTextPasswd;
+    private CheckBox checkBox;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextName = (EditText) findViewById(R.id.et_username);
+        editTextPasswd = (EditText) findViewById(R.id.et_passwd);
+        checkBox = (CheckBox) findViewById(R.id.checkbox_rem);
+        Map<String,String> hashMap = Userinfo.getUserinfo();
+        if(hashMap != null){
+            editTextName.setText(hashMap.get("name"));
+            editTextPasswd.setText(hashMap.get("passwd"));
+            Log.i(TAG,hashMap.get("isChecked"));
+            if ("Remember".equals(hashMap.get("isChecked"))){
+                checkBox.setChecked(true);
+            }
+        }
+        button = (Button) findViewById(R.id.button_login);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String name = editTextName.getText().toString().trim();
+                String passwd = editTextPasswd.getText().toString().trim();
+                String isChecked = checkBox.getText().toString().trim();
+                if(TextUtils.isEmpty(name) || TextUtils.isEmpty(passwd)){
+                    Toast.makeText(MainActivity.this,"UserName or Passwd can not be empty",Toast.LENGTH_LONG).show();
+                }
+
+                if(checkBox.isChecked()){
+                    Log.i(TAG,"SAVE:" + name + " " + passwd + " " + isChecked);
+                    Userinfo.saveUserInfo(name,passwd,isChecked);
+                    Toast.makeText(MainActivity.this,"saved",Toast.LENGTH_LONG).show();
+                }else{
+                        File file = new File("/data/data/com.example.administrator.login/info.txt");
+                        if(file.exists()){
+                            file.delete();
+                            Toast.makeText(MainActivity.this,"deleting",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                Toast.makeText(MainActivity.this,"logining",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
---
+++ day2/Login/app/src/main/java/com/example/administrator/login/Userinfo.java
@@ -0,0 +1,71 @@
+package com.example.administrator.login;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2017/4/16.
+ */
+
+public class Userinfo {
+    private static String TAG = Userinfo.class.getSimpleName();
+    private String mUserName;
+    private String mPasswd;
+    private boolean isChecked;
+    Userinfo(String userName,String passwd){
+        mUserName = userName;
+        mPasswd = passwd;
+    }
+
+    public static boolean saveUserInfo(String name,String passwd,String isChecked) {
+        String result = name + "##" + passwd + "##" + isChecked;
+        try {
+            File file = new File("/data/data/com.example.administrator.login/info.txt");
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            fileOutputStream.write(result.getBytes());
+            fileOutputStream.flush();
+            fileOutputStream.close();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+
+        }
+    }
+    public static Map<String , String > getUserinfo(){
+        try {
+            File file = new File("/data/data/com.example.administrator.login/info.txt");
+            FileInputStream fileInputStream = new FileInputStream(file);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
+            String result = bufferedReader.readLine();
+            if (null == result){
+                return null;
+            }
+            String[] name_passwd = result.split("##");
+            String name = name_passwd[0];
+            String passwd = name_passwd[1];
+            String isChecked = name_passwd[2];
+            HashMap hashMap = new HashMap<String,String>();
+            Log.i(TAG,"get:" + name + " " + passwd + " " + isChecked);
+            hashMap.put("name",name);
+            hashMap.put("passwd",passwd);
+            hashMap.put("isChecked",isChecked);
+            fileInputStream.close();
+            return hashMap;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
